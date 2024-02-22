50dc355f49db041f86bfd47093f0fa55c0cff983
Daniel Macak <daniel.macak@email.cz>
2024-02-22 19:12:38 +0100

    fix: bring gitcommit diff highlighting back
    It was actually caused by treesitter's highlight config. Not sure why,
    but turning it off solved it, and I don't miss it so far. Kudos to
    https://neovim.discourse.group/t/git-diff-highlighting-are-not-working-anymore-in-gitcommit-filetype/3547

--- init.lua
+++ init.lua
@@ -63,17 +63,18 @@ require'onedark'.load()
 
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "css", "lua", "vim", "vimdoc", "query", "tsx", "typescript" },
+  ensure_installed = {
+    "c", "css", "diff", "gitcommit", "git_rebase", "lua", "vim", "vimdoc", "query", "tsx", "typescript" 
+  },
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-  highlight = {
-    enable = true,
-  }
+  -- I commented highlight because it breaks diff highlighting in gitcommit
+  -- highlight = {
+  --   enable = true,
+  -- }
 }
 
 require'nvim-tree'.setup {
